d5101e891e6de528121c52b633ac8a185d8f1117
Gerard Wypych <science.artificer@gmail.com>
2017-12-29 03:22:49 +0100

    Interactvie drawing triangle C++
    Drawing triangle with flat shading in C++.
    I added also moving verticles by mouse.

--- main.cpp
+++ main.cpp
@@ -4,41 +4,168 @@
 #include <chrono>
 #include <iostream>
 #include <iomanip>
+#include <cmath>
 
 using ns = std::chrono::nanoseconds;
 using ms = std::chrono::milliseconds;
 using get_time = std::chrono::steady_clock;
 
-unsigned int width = 1000;
-unsigned int height = 400;
+int width = 1000;
+int height = 600;
 
 struct verticle
 {
-	unsigned int x, y;
+	int x, y;
 	char r,g,b,a;
 };
 
-void drawGrid( sf::Uint8* pixels, unsigned int space = 15  )
+struct verticle_float
 {
-	for( unsigned int j = 0; j < width*height*4; j += 4*width )
-	for( unsigned int i = 0; i < width*4; i += 4*space )
+	float x, y;
+	float r,g,b,a;
+};
+
+void drawGrid( sf::Uint8* pixels, int space = 15  )
+{
+	for( int j = 0; j < width*height*4; j += 4*width )
+	for( int i = 0; i < width*4; i += 4*space )
 	{
 		pixels[i+j+0] = 0;
 		pixels[i+j+1] = 0;
 		pixels[i+j+2] = 0;	
 	}
-	for( unsigned int j = 0; j < width*height*4; j += 4*space*width + 4*width )
-	for( unsigned int i = 0; i < width*4; i += 4 )
+	for( int j = 0; j < width*height*4; j += 4*space*width + 4*width )
+	for( int i = 0; i < width*4; i += 4 )
 	{
 		pixels[j+i+0] = 0;
 		pixels[j+i+1] = 0;
 		pixels[j+i+2] = 0;
 	}
 }
+void swap( verticle& v1, verticle& v2 )
+{
+	verticle tmp = v1;
+	v1 = v2;
+	v2 = tmp;
+}
 
 void drawTriangle( sf::Uint8* pixels, verticle* verticles )
 {
+// BEZ KOLORÓW !!!!!
 
+	// 1. Sortowanie wierzchołków po y (v1.y <= v2.y <= v3.y)
+	
+	verticle v1 = verticles[0];
+	verticle v2 = verticles[1];
+	verticle v3 = verticles[2];
+	
+	if( v2.y < v1.y ) swap( v1, v2 );
+	if( v3.y < v1.y ) swap( v1, v3 );
+	if( v3.y < v2.y ) swap( v2, v3 );
+	//std::cout << v1.y << ' ' << v2.y << ' ' << v3.y << '\n';
+	//std::cout << v1.x << ' ' << v2.x << ' ' << v3.x << '\n';
+	// Obliczanie różnic
+	verticle_float d13, d12, d23;
+	if( v3.y == v1.y ) return;
+	d13.x = float(v3.x - v1.x)/(v3.y - v1.y);
+	
+	//std::cout << d13.x << ' ' << d12.x << ' ' << d23.x << '\n';
+	// Początek i koniec odcinka
+	verticle_float Vb, Ve;
+	Vb.x = v1.x;
+	Vb.y = v1.y;
+	Ve = Vb;
+		
+	// 2. Rysowanie poziomych linii między wierzchołkami v1 i v2
+	// 	a) d13 i d12 - zmiany współrzędnej x i kolorów w stosunku do zmiany y
+	//		b) Vb i Ve - VectorBegin i VectorEnd, czyli (x,y,r,g,b,a) początku i końca rysowanej linii
+	//			wyznaczane za pomocą algorytmu Bresenham'a
+	//		c) Rysowanie poziomej linii przy pomocy interpolacji liniowej
+	//		d) skok do 2b jeśli nie doszliśmy do v2
+	if( v2.y != v1.y )
+	{
+		d12.x = float(v2.x - v1.x)/(v2.y - v1.y);
+		//std::cout << v2.y << " " <<  v1.y << " " << d12.x << "\n";
+		while( Vb.y <= v2.y )
+		{
+			//std::cout << Vb.y << ' ' << Vb.x << ' ' << Ve.x <<'\n';
+			if( Vb.x < Ve.x )
+			{
+				for( int i = std::round(Vb.x); i <= std::round(Ve.x); ++i )
+				{
+					pixels[4*i + int(Vb.y)*width*4 + 0] = 255;
+					pixels[4*i + int(Vb.y)*width*4 + 1] = 0;
+					pixels[4*i + int(Vb.y)*width*4 + 2] = 0;
+					pixels[4*i + int(Vb.y)*width*4 + 3] = 255;
+				}
+			}
+			else
+			{
+				for( int i = std::round(Ve.x); i <= std::round(Vb.x); ++i )
+				{
+					pixels[4*i + int(Vb.y)*width*4 + 0] = 255;
+					pixels[4*i + int(Vb.y)*width*4 + 1] = 0;
+					pixels[4*i + int(Vb.y)*width*4 + 2] = 0;
+					pixels[4*i + int(Vb.y)*width*4 + 3] = 255;
+				}
+			}
+			
+			
+			Vb.x += d13.x;
+			Vb.y += 1;
+			
+			Ve.x += d12.x;
+			Ve.y += 1;
+		}
+		Ve.x -= d12.x;
+	}
+	// 3. Rysowanie poziomych linii między wierzchołkami v2 i v3
+	//		a) d13 i d23 - zmiany współrzędnej x i kolorów w stosunku do zmiany y
+	//		b) Vb i Ve - VectorBegin i VectorEnd, czyli (x,y,r,g,b,a) początku i końca rysowanej linii
+	//			wyznaczane za pomocą algorytmu Bresenham'a
+	//		c) Rysowanie poziomej linii przy pomocy interpolacji liniowej
+	//		d) skok do 3b jeśli nie doszliśmy do v3
+	//
+	
+	
+	if( v3.y != v2.y )
+	{
+		d23.x = float(v3.x - v2.x)/(v3.y - v2.y);
+		Ve.x = v2.x;
+		while( Vb.y <= v3.y )
+		{
+			//std::cout << Vb.y << ' ' << Vb.x << ' ' << Ve.x <<'\n';
+			if( Vb.x < Ve.x )
+			{
+				for( int i = std::round(Vb.x); i <= std::round(Ve.x); ++i )
+				{
+					//std::cout << Vb.x << ' ' << Ve.x << ' ' << Vb.y << ' ' << i << '\n';
+					pixels[4*i + int(Vb.y)*width*4 + 0] = 0;
+					pixels[4*i + int(Vb.y)*width*4 + 1] = 255;
+					pixels[4*i + int(Vb.y)*width*4 + 2] = 0;
+					pixels[4*i + int(Vb.y)*width*4 + 3] = 255;
+				}
+			}
+			else
+			{
+				for( int i = std::round(Ve.x); i <= std::round(Vb.x); ++i )
+				{
+					pixels[4*i + int(Vb.y)*width*4 + 0] = 0;
+					pixels[4*i + int(Vb.y)*width*4 + 1] = 255;
+					pixels[4*i + int(Vb.y)*width*4 + 2] = 0;
+					pixels[4*i + int(Vb.y)*width*4 + 3] = 255;
+				}
+			}
+			
+			
+			Vb.x += d13.x;
+			Vb.y += 1;
+			
+			Ve.x += d23.x;
+			Ve.y += 1;
+		}	
+	}
+	// 4. Koniec
 }
 
 int main()
@@ -105,6 +232,31 @@ int main()
 				sprite->setTexture(texture);
 			}
 		}	
+		delete image;
+		image = new sf::Image;
+		image->create( width, height, sf::Color::White );
+		
+		if( sf::Keyboard::isKeyPressed(sf::Keyboard::Num1) )
+		{
+			if( 0 <= sf::Mouse::getPosition(window).x && sf::Mouse::getPosition(window).x < width )
+				verticles[0].x = sf::Mouse::getPosition(window).x;
+			if( 0 <= sf::Mouse::getPosition(window).y && sf::Mouse::getPosition(window).y < height )
+				verticles[0].y = sf::Mouse::getPosition(window).y;
+		}
+		else if( sf::Keyboard::isKeyPressed(sf::Keyboard::Num2) )
+		{
+			if( 0 <= sf::Mouse::getPosition(window).x && sf::Mouse::getPosition(window).x < width )
+				verticles[1].x = sf::Mouse::getPosition(window).x;
+			if( 0 <= sf::Mouse::getPosition(window).y && sf::Mouse::getPosition(window).y < height )
+				verticles[1].y = sf::Mouse::getPosition(window).y;
+		}
+		else if( sf::Keyboard::isKeyPressed(sf::Keyboard::Num3) )
+		{
+			if( 0 <= sf::Mouse::getPosition(window).x && sf::Mouse::getPosition(window).x < width )
+				verticles[2].x = sf::Mouse::getPosition(window).x;
+			if( 0 <= sf::Mouse::getPosition(window).y && sf::Mouse::getPosition(window).y < height )
+				verticles[2].y = sf::Mouse::getPosition(window).y;
+		}
 		
 		start = get_time::now();
 
@@ -120,7 +272,7 @@ int main()
 
 		start = get_time::now();
 		
-		x86_function( (sf::Uint8*)image->getPixelsPtr(), verticles );
+		x86_function( (sf::Uint8*)image->getPixelsPtr(), width, height, verticles );
 
 		std::cout << "drawTriangle_x86: " << std::setw(20) << std::right << (std::chrono::duration_cast<ns>(get_time::now() - start)).count() << " ns\n";
 
